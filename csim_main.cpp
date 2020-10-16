e2646385301f0b2784b3a9bc6df946d8f4667c74
Sam Lipschitz <samlipschitz@Sams-MacBook-Pro.local>
2020-10-16 12:15:54 -0400

    added checks for correct int params

--- csim_main.cpp
+++ csim_main.cpp
@@ -9,15 +9,44 @@ struct Cache{
  //TODO: define structure
 };
 
+int isValid(int val){
+    if(val <= 0){
+        return 0;
+    }
+    return (val & (val-1)) == 0;
 
-int main(int argc, char *argv[]) {
-    if (argc != 6) {
-        printf("Argument count wrong.");
+}
+
+int allValuesValid(int sets, int blocks, int bytes){
+    if(!isValid(sets)){
+        printf("Invalid set value.\n");
         return 1;
     }
-    int sets = *argv[1];
-    int blocks = *argv[2];
-    int bytes = *argv[3];
+    if(!isValid(blocks) || blocks < 4){
+        printf("Invalid block value.\n");
+        return 1;
+    }
+
+    if(!isValid(bytes)){
+        printf("Invalid byte value.\n");
+        return 1;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 7) {
+        printf("Argument count wrong.\n");
+        return 1;
+    }
+
+    int sets = std::stoi(argv[1]);
+    int blocks = std::stoi(argv[2]);
+    int bytes = std::stoi(argv[3]);
+    if(!allValuesValid(sets, blocks, bytes)){
+        return 1;
+    }
+
 
 
     return 0;
